c2216c0890086a9737a330b860c703619b42ef8d
Zach Oines <zachoines@gmail.com>
2019-06-21 19:01:02 -0500

    Added model loading and saving.

--- Coordinator.py
+++ Coordinator.py
@@ -37,121 +37,129 @@ class Coordinator:
 
     def run(self):
         
-        # Main training loop
-        for epoch in range(self.num_epocs):
+        try:
+            # Main training loop
+            for epoch in range(self.num_epocs):
 
-            # ready workers for the next epoc
-            for worker in self.workers:
-                worker.reset()
-
-            # loop for generating a training session a batch at a time
-            for mb in range(self.num_minibatches):
-
-                # Copy global network over to local network
-                self.model.refresh_local_network_params()
-
-                # Send workers out to threads
-                threads = []
+                # ready workers for the next epoc
                 for worker in self.workers:
-                    threads.append(WorkerThread(target=worker.run, args=()))
+                    worker.reset()
 
-                # Start the workers on their tasks
-                for thread in threads:
-                    thread.start()
+                # loop for generating a training session a batch at a time
+                for mb in range(self.num_minibatches):
+
+                    # Copy global network over to local network
+                    self.model.refresh_local_network_params()
+
+                    # Send workers out to threads
+                    threads = []
+                    for worker in self.workers:
+                        threads.append(WorkerThread(target=worker.run, args=()))
+
+                    # Start the workers on their tasks
+                    for thread in threads:
+                        thread.start()
+                        
+                    batches = []
                     
-                batches = []
-                
-                # Wait foreach worker to finish and return their batch
-                for thread in threads:
-                    batches.append(thread.join())
+                    # Wait foreach worker to finish and return their batch
+                    for thread in threads:
+                        batches.append(thread.join())
 
-                all_batches_discounted_rewards = np.array([])
-                all_batches_advantages = np.array([])
-                all_batches_values = np.array([])
-                all_batches_actions = np.array([])
-                all_batches_loss = np.array([])
-                all_batches_observations = np.array([])
-                all_batches_states = []
-                all_batches_rewards = np.array([])
-                
-                # Calculate discounted rewards for each environment
-                for env in range(self.num_envs):
-                    done = False
-                    bootstrap_value = 0
-                    total_discounted_rewards = 0
-                    steps = 0
-                    batch_advantages = np.array([])
-                    batch_rewards = []
-                    batch_values = np.array([])
-                    batch_dones = []
-                    batch_observations = []
-                    batch_states = []
-                    batch_actions = []
-
-                    mb = batches[env]
-
-                    # For every step made in this env for this particular batch
-                    for step in mb:
-                        steps += 1
-                        (state, observation, reward, value, action, done) = step
-                        batch_rewards.append(reward)
-                        batch_values = np.append(batch_values, value)
-                        batch_dones.append(done)
-                        batch_observations.append(observation)
-                        batch_states.append(state)
-                        batch_actions.append(action)
-
-                        # If we reached the end of an episode or if we filled a batch without reaching termination of episode
-                        # we boot-strap the final rewards with the V_s(last_observation)
-
-                        # Discounted bootstraped rewards formula:
-                        # G_t == R + γ * V(S_t'):
-
-                        # Advantage
-                        # δ_t == R + γ * V(S_t') - V(S_t): 
-                        if (steps % self.batch_size == 0):
-                            
-                            # Bootstrap terminal state value onto list of discounted retur                    batch_rewards = adjusted_discounterd_rewards(batch_rewards)
-                            batch_rewards = self.discounted_rewards(batch_rewards + [0.0], batch_dones, self.gamma)
-                            V_S = np.append(batch_values[:-1], 0.0)
-                            V_next = np.append(np.array(0.0), batch_values[1:])
-                            batch_advantages = batch_rewards + self.gamma * ((V_next) - (V_S))
-                            break
-                        elif done:
-                            
-                            # Generate a reversed dicounted list of returns without boostrating (adding V(s_terminal)) on non-terminal state
-                            batch_rewards = self.discounted_rewards(batch_rewards + [0.0], batch_dones, self.gamma)
-                            V_S = np.append(batch_values[:-1], 0.0)
-                            V_next = np.append(np.array(0.0), batch_values[1:])
-                            batch_advantages = batch_rewards + self.gamma * ((V_next) - (V_S))
-                            break
-                        else:
-                            
-                            # Continue accumulating batch data
-                            continue
-
+                    all_batches_discounted_rewards = np.array([])
+                    all_batches_advantages = np.array([])
+                    all_batches_values = np.array([])
+                    all_batches_actions = np.array([])
+                    all_batches_loss = np.array([])
+                    all_batches_observations = np.array([])
+                    all_batches_states = []
+                    all_batches_rewards = np.array([])
                     
+                    # Calculate discounted rewards for each environment
+                    for env in range(self.num_envs):
+                        done = False
+                        bootstrap_value = 0
+                        total_discounted_rewards = 0
+                        steps = 0
+                        batch_advantages = np.array([])
+                        batch_rewards = []
+                        batch_values = np.array([])
+                        batch_dones = []
+                        batch_observations = []
+                        batch_states = []
+                        batch_actions = []
+
+                        mb = batches[env]
+
+                        # For every step made in this env for this particular batch
+                        for step in mb:
+                            steps += 1
+                            (state, observation, reward, value, action, done) = step
+                            batch_rewards.append(reward)
+                            batch_values = np.append(batch_values, value)
+                            batch_dones.append(done)
+                            batch_observations.append(observation)
+                            batch_states.append(state)
+                            batch_actions.append(action)
+
+                            # If we reached the end of an episode or if we filled a batch without reaching termination of episode
+                            # we boot-strap the final rewards with the V_s(last_observation)
+
+                            # Discounted bootstraped rewards formula:
+                            # G_t == R + γ * V(S_t'):
+
+                            # Advantage
+                            # δ_t == R + γ * V(S_t') - V(S_t): 
+                            if (steps % self.batch_size == 0):
+                                
+                                # Bootstrap terminal state value onto list of discounted retur                    batch_rewards = adjusted_discounterd_rewards(batch_rewards)
+                                batch_rewards = self.discounted_rewards(batch_rewards + [0.0], batch_dones, self.gamma)
+                                V_S = np.append(batch_values[:-1], 0.0)
+                                V_next = np.append(np.array(0.0), batch_values[1:])
+                                batch_advantages = batch_rewards + self.gamma * ((V_next) - (V_S))
+                                break
+                            elif done:
+                                
+                                # Generate a reversed dicounted list of returns without boostrating (adding V(s_terminal)) on non-terminal state
+                                batch_rewards = self.discounted_rewards(batch_rewards + [0.0], batch_dones, self.gamma)
+                                V_S = np.append(batch_values[:-1], 0.0)
+                                V_next = np.append(np.array(0.0), batch_values[1:])
+                                batch_advantages = batch_rewards + self.gamma * ((V_next) - (V_S))
+                                break
+                            else:
+                                
+                                # Continue accumulating batch data
+                                continue
+
+                        
 
 
-                    # Collect all individual batch data from each env
-                    all_batches_values = np.concatenate((all_batches_values, batch_values), axis=0)
-                    all_batches_discounted_rewards = np.concatenate((all_batches_discounted_rewards, batch_rewards), axis = 0)
-                    all_batches_advantages = np.concatenate((all_batches_advantages, batch_advantages), axis = 0)
-                    all_batches_actions = np.concatenate((all_batches_actions, batch_actions), axis = 0)
+                        # Collect all individual batch data from each env
+                        all_batches_values = np.concatenate((all_batches_values, batch_values), axis=0)
+                        all_batches_discounted_rewards = np.concatenate((all_batches_discounted_rewards, batch_rewards), axis = 0)
+                        all_batches_advantages = np.concatenate((all_batches_advantages, batch_advantages), axis = 0)
+                        all_batches_actions = np.concatenate((all_batches_actions, batch_actions), axis = 0)
+                        
+                        # all_batches_observations = np.concatenate((all_batches_observations, batch_observations), axis = 0)
+                        all_batches_states += batch_states
+                        all_batches_rewards = np.concatenate((all_batches_rewards, batch_rewards), axis = 0)
+
+                    # Now perform tensorflow session to determine policy and value loss for this batch
+                    feed_dict = { 
+                        self.model.step_policy.X_input: all_batches_states,
+                        self.model.step_policy.actions: all_batches_actions,
+                        self.model.step_policy.advantages: all_batches_advantages,
+                        self.model.step_policy.values: all_batches_values,
+                        self.model.step_policy.rewards: all_batches_rewards, 
+                    }
                     
-                    # all_batches_observations = np.concatenate((all_batches_observations, batch_observations), axis = 0)
-                    all_batches_states += batch_states
-                    all_batches_rewards = np.concatenate((all_batches_rewards, batch_rewards), axis = 0)
-
-                # Now perform tensorflow session to determine policy and value loss for this batch
-                feed_dict = { 
-                    self.model.step_policy.X_input: all_batches_states,
-                    self.model.step_policy.actions: all_batches_actions,
-                    self.model.step_policy.advantages: all_batches_advantages,
-                    self.model.step_policy.values: all_batches_values,
-                    self.model.step_policy.rewards: all_batches_rewards, 
-                }
-                
-                # Run tensorflow graph, return loss without updateing gradients 
-                loss, optimizer = self.sess.run([self.model.step_policy.loss, self.model.step_policy.optimize], feed_dict)
+                    # Run tensorflow graph, return loss without updateing gradients 
+                    loss, optimizer = self.sess.run([self.model.step_policy.loss, self.model.step_policy.optimize], feed_dict)
+            print("Training session was sucessfull.")
+            return True
+        except:
+            print("ERROR: The coordinator ran into an issue during training!")
+            raise  
+            return False
+        
 
--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # Utility python classes
 import threading
+import os
 import numpy as np
 import tensorflow as tf
 import cv2
@@ -30,11 +31,16 @@ env_4 = 'SuperMarioBros2-v0'
 # env_names = [env_1, env_2, env_3, env_4]
 env_names = [env_1]
 
+# Configuration
+current_dir = os.getcwd()
+model_save_path = current_dir + '\AC2_MARIO\Model'
+video_save_path = current_dir + '\AC2_MARIO\Videos'
+
 # Enviromental vars
 num_envs = len(env_names)
 batch_size = 32
 num_minibatches = 8
-num_epocs = 16
+num_epocs = 1
 gamma = .99
 learning_rate =  7e-4
 
@@ -68,13 +74,37 @@ ACTION_SPACE = envs[0].env.action_space
 NUM_STATE = (1, HEIGHT, WIDTH, CHANNELS)
 
 
-
 # Init the Network, model, and Workers, starting them onto their own thread
 network_params = (sess, NUM_STATE, batch_size, NUM_ACTIONS, ACTION_SPACE)
 model = Model(network_params)
 sess.run(tf.global_variables_initializer())
 
-workers = [Worker(model, env, batch_size = 32, render = False) for env in envs]
 
+# Load model if exists
+saver = tf.train.Saver()
+if not os.path.exists(model_save_path):
+    os.makedirs(model_save_path)
+else:
+    try:
+        saver.restore(sess, model_save_path + "\model.ckpt")
+        print("Model restored.")
+    except:
+        print("ERROR: There was an issue loading the model!")
+        raise
+
+if not os.path.exists(video_save_path):
+    os.makedirs(video_save_path)
+
+# Init coordinator and send out the workers
+workers = [Worker(model, env, batch_size = 32, render = False) for env in envs]
 coordinator = Coordinator(sess, model, workers, num_envs, num_epocs, num_minibatches, batch_size, gamma)
-coordinator.run()
+
+# Train and save
+if coordinator.run():
+    try:
+        save_path = saver.save(sess, model_save_path + "\model.ckpt")
+        print("Model saved.")
+    except:
+        print("ERROR: There was an issue saving the model!")
+        raise
+    
