Fixed logic error's in advantage calculation, remade NN Layers.py file with my own simplified layers.

AC_Network.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
-from layer import conv2d, flatten, dense
-from layer import orthogonal_initializer, noise_and_argmax
+from Layers import create_conv, flatten, create_dense
+from Layers import orthogonal_initializer, noise_and_argmax
 
 
 
@@ -28,28 +28,28 @@ class AC_Network:
 
         with tf.variable_scope(name):
             self.X_input = tf.placeholder(tf.float32, (None, 96, 96, 1))
-            conv1 = conv2d('conv1', self.X_input, num_filters=32, kernel_size=(8, 8),
+            conv1 = create_conv('conv1', self.X_input, num_filters=32, kernel_size=(8, 8),
                            padding='VALID', stride=(4, 4),
                            initializer=orthogonal_initializer(np.sqrt(2)), activation=tf.nn.relu,
                            is_training=is_training)
 
-            conv2 = conv2d('conv2', conv1, num_filters=64, kernel_size=(4, 4), padding='VALID', stride=(2, 2),
+            conv2 = create_conv('conv2', conv1, num_filters=64, kernel_size=(4, 4), padding='VALID', stride=(2, 2),
                            initializer=orthogonal_initializer(np.sqrt(2)), activation=tf.nn.relu,
                            is_training=is_training)
 
-            conv3 = conv2d('conv3', conv2, num_filters=64, kernel_size=(3, 3), padding='VALID', stride=(1, 1),
+            conv3 = create_conv('conv3', conv2, num_filters=64, kernel_size=(3, 3), padding='VALID', stride=(1, 1),
                            initializer=orthogonal_initializer(np.sqrt(2)), activation=tf.nn.relu,
                            is_training=is_training)
 
             conv3_flattened = flatten(conv3)
 
-            fc4 = dense('fc4', conv3_flattened, output_dim = 512, initializer = orthogonal_initializer(np.sqrt(2)),
+            fc4 = create_dense('fc4', conv3_flattened, output_dim = 512, initializer = orthogonal_initializer(np.sqrt(2)),
                         activation=tf.nn.relu, is_training=is_training)
 
-            self.policy_logits = dense('policy_logits', fc4, output_dim = num_actions,
+            self.policy_logits = create_dense('policy_logits', fc4, output_dim = num_actions,
                                        initializer = orthogonal_initializer(np.sqrt(1.0)), is_training=is_training)
 
-            self.value_function = dense('value_function', fc4, output_dim=1,
+            self.value_function = create_dense('value_function', fc4, output_dim=1,
                                         initializer = orthogonal_initializer(np.sqrt(1.0)), is_training=is_training)
 
             with tf.name_scope('value'):

Coordinator.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 import cv2
+import scipy.signal
 
 # import local classes
 from Model import Model
@@ -28,6 +29,9 @@ class Coordinator:
             r = reward + gamma * r * (1. - done)  # fixed off by one bug
             discounted.append(r)
         return np.array(discounted[::-1])
+
+    def discount(self, x, gamma):
+        return scipy.signal.lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
 
     # for debugging processed images
     def displayImage(self, img):
@@ -90,6 +94,7 @@ class Coordinator:
                         batch_actions = []
 
                         mb = batches[env]
+                        collected = False
 
                         # For every step made in this env for this particular batch
                         for step in mb:
@@ -110,39 +115,53 @@ class Coordinator:
 
                             # Advantage
                             # δ_t == R + γ * V(S_t') - V(S_t): 
-                            if (steps % self.batch_size == 0):
+                            if (steps % self.batch_size == 0 and not done):
                                 
                                 # Bootstrap terminal state value onto list of discounted retur                    batch_rewards = adjusted_discounterd_rewards(batch_rewards)
-                                batch_rewards = self.discounted_rewards(batch_rewards + [0.0], batch_dones, self.gamma)
-                                V_S = np.append(batch_values[:-1], 0.0)
-                                V_next = np.append(np.array(0.0), batch_values[1:])
-                                batch_advantages = batch_rewards + self.gamma * ((V_next) - (V_S))
-                                break
-                            elif done:
+                                # batch_rewards = self.discounted_rewards(batch_rewards, batch_dones, self.gamma)
+                                # V_S = np.append(batch_values[:-1], 0.0)
+                                # V_next = np.append(np.array(0.0), batch_values[1:])
+                                # batch_advantages = (batch_rewards + self.gamma * (V_next)) - (V_S)
+
+                                batch_rewards = self.discount(batch_rewards, self.gamma)  
+                                batch_advantages = batch_rewards - batch_values
                                 
-                                # Generate a reversed dicounted list of returns without boostrating (adding V(s_terminal)) on non-terminal state
-                                batch_rewards = self.discounted_rewards(batch_rewards + [0.0], batch_dones, self.gamma)
-                                V_S = np.append(batch_values[:-1], 0.0)
-                                V_next = np.append(np.array(0.0), batch_values[1:])
-                                batch_advantages = batch_rewards + self.gamma * ((V_next) - (V_S))
+                                index = self.batch_size - 1
+                                bootstrap = self.gamma * self.model.value([observation])
+                                batch_advantages[index] += bootstrap
+
+                                # Collect all individual batch data from each env
+                                all_batches_values = np.concatenate((all_batches_values, batch_values), axis=0)
+                                all_batches_discounted_rewards = np.concatenate((all_batches_discounted_rewards, batch_rewards), axis = 0)
+                                all_batches_advantages = np.concatenate((all_batches_advantages, batch_advantages), axis = 0)
+                                all_batches_actions = np.concatenate((all_batches_actions, batch_actions), axis = 0)
+                                
+                                # all_batches_observations = np.concatenate((all_batches_observations, batch_observations), axis = 0)
+                                all_batches_states += batch_states
+                                all_batches_rewards = np.concatenate((all_batches_rewards, batch_rewards), axis = 0)
+                                collected = True
                                 break
+                         
                             else:
                                 
                                 # Continue accumulating batch data
                                 continue
-
                         
-
-
-                        # Collect all individual batch data from each env
-                        all_batches_values = np.concatenate((all_batches_values, batch_values), axis=0)
-                        all_batches_discounted_rewards = np.concatenate((all_batches_discounted_rewards, batch_rewards), axis = 0)
-                        all_batches_advantages = np.concatenate((all_batches_advantages, batch_advantages), axis = 0)
-                        all_batches_actions = np.concatenate((all_batches_actions, batch_actions), axis = 0)
+                        batch_rewards = self.discount(batch_rewards, self.gamma)  
+                        batch_advantages = batch_rewards - batch_values
                         
-                        # all_batches_observations = np.concatenate((all_batches_observations, batch_observations), axis = 0)
-                        all_batches_states += batch_states
-                        all_batches_rewards = np.concatenate((all_batches_rewards, batch_rewards), axis = 0)
+                        if not collected:
+
+                            # Collect all individual batch data from each env
+                            all_batches_values = np.concatenate((all_batches_values, batch_values), axis=0)
+                            all_batches_discounted_rewards = np.concatenate((all_batches_discounted_rewards, batch_rewards), axis = 0)
+                            all_batches_advantages = np.concatenate((all_batches_advantages, batch_advantages), axis = 0)
+                            all_batches_actions = np.concatenate((all_batches_actions, batch_actions), axis = 0)
+                            
+                            # all_batches_observations = np.concatenate((all_batches_observations, batch_observations), axis = 0)
+                            all_batches_states += batch_states
+                            all_batches_rewards = np.concatenate((all_batches_rewards, batch_rewards), axis = 0)
+                            
 
                     # Now perform tensorflow session to determine policy and value loss for this batch
                     feed_dict = { 

Worker.py
@@ -34,6 +34,8 @@ class Worker():
         self.batch_size = batch_size 
         self.s = None
         self.NUM_ACTIONS = env.action_space.n
+        self.NONE_STATE = np.zeros(self.env.observation_space.shape)
+        
 
     # Reset worker and evironment variables in preperation for a new epoc
     def reset(self):
@@ -42,7 +44,7 @@ class Worker():
         [self.s] = self.env.reset()
 
 
-    # Generate an epocs worth of observations. Return nothing.
+    # Generate an batch worth of observations. Return nothing.
     def run(self):
         batch = []
         for step in range(self.batch_size):
@@ -54,15 +56,20 @@ class Worker():
                 [s_t], reward, d, _ = self.env.step(action)
                 self._done = d
 
+
                 batch.append((self.s, s_t , reward, value, action, d))
                 s = [s_t]
 
                 # render the env
                 if (self._render):
                     self.env.render()
+            
             # if the episode is already _done, generate a null entry
             else:
-                batch.append((None, None, 0, 0, 0, True))
+                
+                print("Mario died in a thread.")
+                batch.append(([self.NONE_STATE], [self.NONE_STATE], 0, 0, 0, True))
+
         self._batch_buffer.append(batch)
         return batch
 

layer.py
@@ -39,68 +39,7 @@ def conv2d_p(name, x, w=None, num_filters=16, kernel_size=(3, 3), padding='SAME'
     return out
 
 
-def atrous_conv2d_p(name, x, w=None, num_filters=16, kernel_size=(3, 3), padding='SAME', dilation_rate=1,
-                    initializer=tf.contrib.layers.xavier_initializer(), l2_strength=0.0, bias=0.0):
-    """
-    Atrous Convolution 2D Wrapper
-    :param name: (string) The name scope provided by the upper tf.name_scope('name') as scope.
-    :param x: (tf.tensor) The input to the layer (N, H, W, C).
-    :param w: (tf.tensor) pretrained weights
-    :param num_filters: (integer) No. of filters (This is the output depth)
-    :param kernel_size: (integer tuple) The size of the convolving kernel.
-    :param padding: (string) The amount of padding required.
-    :param dilation_rate: (integer) The amount of dilation required. If equals 1, it means normal convolution.
-    :param initializer: (tf.contrib initializer) The initialization scheme, He et al. normal or Xavier normal are recommended.
-    :param l2_strength:(weight decay) (float) L2 regularization parameter.
-    :param bias: (float) Amount of bias. (if not float, it means pretrained bias)
-    :return out: The output of the layer. (N, H', W', num_filters)
-    """
-    with tf.variable_scope(name):
-        kernel_shape = [kernel_size[0], kernel_size[1], x.shape[-1], num_filters]
 
-        with tf.name_scope('layer_weights'):
-            if w == None:
-                w = variable_with_weight_decay(kernel_shape, initializer, l2_strength)
-            variable_summaries(w)
-        with tf.name_scope('layer_biases'):
-            if isinstance(bias, float):
-                bias = tf.get_variable('biases', [num_filters], initializer=tf.constant_initializer(bias))
-            variable_summaries(bias)
-        with tf.name_scope('layer_atrous_conv2d'):
-            conv = tf.nn.atrous_conv2d(x, w, dilation_rate, padding)
-            out = tf.nn.bias_add(conv, bias)
-
-    return out
-
-
-def conv2d_transpose_p(name, x, w=None, output_shape=None, kernel_size=(3, 3), padding='SAME', stride=(1, 1),
-                       l2_strength=0.0,
-                       bias=0.0):
-    """
-    Convolution Transpose 2D Wrapper
-    :param name: (string) The name scope provided by the upper tf.name_scope('name') as scope.
-    :param x: (tf.tensor) The input to the layer (N, H, W, C).
-    :param output_shape: (Array) [N, H', W', C'] The shape of the corresponding output.
-    :param kernel_size: (integer tuple) The size of the convolving kernel.
-    :param padding: (string) The amount of padding required.
-    :param stride: (integer tuple) The stride required.
-    :param l2_strength:(weight decay) (float) L2 regularization parameter.
-    :param bias: (float) Amount of bias. (if not float, it means pretrained bias)
-    :return out: The output of the layer. (output_shape[0], output_shape[1], output_shape[2], output_shape[3])
-    """
-    with tf.variable_scope(name):
-        stride = [1, stride[0], stride[1], 1]
-        kernel_shape = [kernel_size[0], kernel_size[1], output_shape[-1], x.shape[-1]]
-        if w == None:
-            w = get_deconv_filter(kernel_shape, l2_strength)
-        variable_summaries(w)
-        deconv = tf.nn.conv2d_transpose(x, w, tf.stack(output_shape), strides=stride, padding=padding)
-        if isinstance(bias, float):
-            bias = tf.get_variable('layer_biases', [output_shape[-1]], initializer=tf.constant_initializer(bias))
-        variable_summaries(bias)
-        out = tf.nn.bias_add(deconv, bias)
-
-    return out
 
 
 def conv2d(name, x, w=None, num_filters=16, kernel_size=(3, 3), padding='SAME', stride=(1, 1),
@@ -154,107 +93,6 @@ def conv2d(name, x, w=None, num_filters=16, kernel_size=(3, 3), padding='SAME', 
 
     return conv_o
 
-
-def atrous_conv2d(name, x, w=None, num_filters=16, kernel_size=(3, 3), padding='SAME', dilation_rate=1,
-                  initializer=tf.contrib.layers.xavier_initializer(), l2_strength=0.0, bias=0.0,
-                  activation=None, batchnorm_enabled=False, max_pool_enabled=False, dropout_keep_prob=-1,
-                  is_training=True):
-    """
-    This block is responsible for a Dilated convolution 2D layer followed by optional (non-linearity, dropout, max-pooling).
-    Note that: "is_training" should be passed by a correct value based on being in either training or testing.
-    :param name: (string) The name scope provided by the upper tf.name_scope('name') as scope.
-    :param x: (tf.tensor) The input to the layer (N, H, W, C).
-    :param num_filters: (integer) No. of filters (This is the output depth)
-    :param kernel_size: (integer tuple) The size of the convolving kernel.
-    :param padding: (string) The amount of padding required.
-    :param dilation_rate: (integer) The amount of dilation required. If equals 1, it means normal convolution.
-    :param initializer: (tf.contrib initializer) The initialization scheme, He et al. normal or Xavier normal are recommended.
-    :param l2_strength:(weight decay) (float) L2 regularization parameter.
-    :param bias: (float) Amount of bias.
-    :param activation: (tf.graph operator) The activation function applied after the convolution operation. If None, linear is applied.
-    :param batchnorm_enabled: (boolean) for enabling batch normalization.
-    :param max_pool_enabled:  (boolean) for enabling max-pooling 2x2 to decrease width and height by a factor of 2.
-    :param dropout_keep_prob: (float) for the probability of keeping neurons. If equals -1, it means no dropout
-    :param is_training: (boolean) to diff. between training and testing (important for batch normalization and dropout)
-    :return: The output tensor of the layer (N, H', W', C').
-    """
-    with tf.variable_scope(name) as scope:
-        conv_o_b = atrous_conv2d_p(scope, x=x, w=w, num_filters=num_filters, kernel_size=kernel_size,
-                                   padding=padding, dilation_rate=dilation_rate,
-                                   initializer=initializer, l2_strength=l2_strength, bias=bias)
-
-        if batchnorm_enabled:
-            conv_o_bn = tf.layers.batch_normalization(conv_o_b, training=is_training)
-            if not activation:
-                conv_a = conv_o_bn
-            else:
-                conv_a = activation(conv_o_bn)
-        else:
-            if not activation:
-                conv_a = conv_o_b
-            else:
-                conv_a = activation(conv_o_b)
-
-        if dropout_keep_prob != -1:
-            conv_o_dr = tf.nn.dropout(conv_a, dropout_keep_prob)
-        else:
-            conv_o_dr = conv_a
-
-        conv_o = conv_o_dr
-        if max_pool_enabled:
-            conv_o = max_pool_2d(scope, conv_o_dr)
-
-    return conv_o
-
-
-def conv2d_transpose(name, x, w=None, output_shape=None, kernel_size=(3, 3), padding='SAME', stride=(1, 1),
-                     l2_strength=0.0,
-                     bias=0.0, activation=None, batchnorm_enabled=False, dropout_keep_prob=-1,
-                     is_training=True):
-    """
-    This block is responsible for a convolution transpose 2D followed by optional (non-linearity, dropout, max-pooling).
-    Note that: "is_training" should be passed by a correct value based on being in either training or testing.
-    :param name: (string) The name scope provided by the upper tf.name_scope('name') as scope.
-    :param x: (tf.tensor) The input to the layer (N, H, W, C).
-    :param output_shape: (Array) [N, H', W', C'] The shape of the corresponding output.
-    :param kernel_size: (integer tuple) The size of the convolving kernel.
-    :param padding: (string) The amount of padding required.
-    :param stride: (integer tuple) The stride required.
-    :param l2_strength:(weight decay) (float) L2 regularization parameter.
-    :param bias: (float) Amount of bias.
-    :param activation: (tf.graph operator) The activation function applied after the convolution operation. If None, linear is applied.
-    :param batchnorm_enabled: (boolean) for enabling batch normalization.
-    :param max_pool_enabled:  (boolean) for enabling max-pooling 2x2 to decrease width and height by a factor of 2.
-    :param dropout_keep_prob: (float) for the probability of keeping neurons. If equals -1, it means no dropout
-    :param is_training: (boolean) to diff. between training and testing (important for batch normalization and dropout)
-    :return out: The output of the layer. (output_shape[0], output_shape[1], output_shape[2], output_shape[3])
-    """
-    with tf.variable_scope(name) as scope:
-        conv_o_b = conv2d_transpose_p(name=scope, x=x, w=w, output_shape=output_shape, kernel_size=kernel_size,
-                                      padding=padding, stride=stride,
-                                      l2_strength=l2_strength,
-                                      bias=bias)
-
-        if batchnorm_enabled:
-            conv_o_bn = tf.layers.batch_normalization(conv_o_b, training=is_training)
-            if not activation:
-                conv_a = conv_o_bn
-            else:
-                conv_a = activation(conv_o_bn)
-        else:
-            if not activation:
-                conv_a = conv_o_b
-            else:
-                conv_a = activation(conv_o_b)
-
-        if dropout_keep_prob != -1:
-            conv_o_dr = tf.nn.dropout(conv_a, dropout_keep_prob)
-        else:
-            conv_o_dr = conv_a
-
-        conv_o = conv_o_dr
-
-        return conv_o
 
 
 #############################################################################################################
@@ -341,9 +179,6 @@ def flatten(x):
     return o
 
 
-#############################################################################################################
-# Pooling Layers methods
-
 def max_pool_2d(x, size=(2, 2)):
     """
     Max pooling 2D Wrapper
@@ -356,21 +191,7 @@ def max_pool_2d(x, size=(2, 2)):
                           name='pooling')
 
 
-def upsample_2d(x, size=(2, 2)):
-    """
-    Bilinear Upsampling 2D Wrapper
-    :param x: (tf.tensor) The input to the layer (N,H,W,C).
-    :param size: (tuple) This specifies the size of the filter as well as the stride.
-    :return: The output is the same input but doubled in both width and height (N,2H,2W,C).
-    """
-    h, w, _ = x.get_shape().as_list()[1:]
-    size_x, size_y = size
-    output_h = h * size_x
-    output_w = w * size_y
-    return tf.image.resize_bilinear(x, (output_h, output_w), align_corners=None, name='upsampling')
-
-
-#############################################################################################################
+##########################################################################
 # Utils for Layers methods
 
 def variable_with_weight_decay(kernel_shape, initializer, wd):
@@ -408,29 +229,6 @@ def variable_summaries(var):
         tf.summary.scalar('min', tf.reduce_min(var))
         tf.summary.histogram('histogram', var)
 
-
-def get_deconv_filter(f_shape, l2_strength):
-    """
-    The initializer for the bilinear convolution transpose filters
-    :param f_shape: The shape of the filter used in convolution transpose.
-    :param l2_strength: L2 regularization parameter.
-    :return weights: The initialized weights.
-    """
-    width = f_shape[0]
-    height = f_shape[0]
-    f = math.ceil(width / 2.0)
-    c = (2 * f - 1 - f % 2) / (2.0 * f)
-    bilinear = np.zeros([f_shape[0], f_shape[1]])
-    for x in range(width):
-        for y in range(height):
-            value = (1 - abs(x / f - c)) * (1 - abs(y / f - c))
-            bilinear[x, y] = value
-    weights = np.zeros(f_shape)
-    for i in range(f_shape[2]):
-        weights[:, :, i, i] = bilinear
-
-    init = tf.constant_initializer(value=weights, dtype=tf.float32)
-    return variable_with_weight_decay(weights.shape, init, l2_strength)
 
 
 def noise_and_argmax(logits):