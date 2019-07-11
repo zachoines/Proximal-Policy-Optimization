0de9e01e907184ac8f4c053c3471412369b28875
unknown <Administrator@MSP01-XMJ02PGMW.go.mts.com>
2019-07-11 17:40:46 -0500

    Modified plotting function

--- stats/plot.py
+++ stats/plot.py
@@ -3,19 +3,41 @@ import numpy as np
 
 from numpy import arange,array,ones
 from scipy import stats
+from heapq import *
 
 
 # type what you want to display here
 dimension = "LOSS"
+heap = []
 
 x, runtime, y = np.loadtxt(dimension + ".txt", delimiter=',', unpack=True)
 
-# Best Fit line
-slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-line = slope * x + intercept
-plt.plot(x,y, label='Slope: ' + str(slope))
+for i in range(len(x)):
+    data = y[i]
+    time = x[i]
+    item = (time, data)
+    heappush(heap, item)
 
-plt.plot(x, y,'o', x, line)
+    
+sorted_x = []
+sorted_y = []
+counter = 0
+while heap:
+    (x_i, y_i) = heappop(heap)
+    sorted_x.append(counter)
+    sorted_y.append(y_i)
+    counter += 1
+
+
+# TODO::Statistically find outliers in a data set and exclude then from. IQR
+sorted_x = np.array(sorted_x, dtype = int)
+
+# Best Fit line
+slope, intercept, r_value, p_value, std_err = stats.linregress(sorted_x, sorted_y)
+line = slope * sorted_x + intercept
+plt.plot(sorted_x, sorted_y, label='Slope: ' + str(slope))
+
+plt.plot(sorted_x, sorted_y,'o', sorted_x, line)
 
 plt.xlabel('Time')
 plt.ylabel(dimension)
