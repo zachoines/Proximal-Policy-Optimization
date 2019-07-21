Fixed Loss function and backprop step

AC_Network.py
@@ -68,7 +68,7 @@ class AC_Model(tf.keras.Model):
         # Output Layer consisting of an Actor and a Critic
         self._value = tf.keras.layers.Dense(
             1,
-            kernel_initializer=keras.initializers.Orthogonal(gain=.7, seed=None),
+            kernel_initializer=keras.initializers.Orthogonal(gain=2.0, seed=None),
             activation='linear',
             name="value_layer")
    
@@ -117,221 +117,70 @@ class AC_Model(tf.keras.Model):
 
         with tf.GradientTape() as tape:
             
-            batch_states, batch_actions, batch_advantages, batch_rewards, batch_values = train_data
+            batch_states, batch_actions, batch_advantages, batch_rewards, batch_values, batch_logits = train_data
 
             states = tf.Variable(batch_states)
             actions = tf.Variable(batch_actions)
             advantages = tf.Variable(batch_advantages)
             rewards = tf.Variable(batch_rewards)
+            logits = tf.Variable(batch_logits)
             tape.watched_variables()
-
             actions_hot = tf.one_hot(actions, 7, dtype=tf.float32)
+
+            # Entropy: - (1 / n) * ∑ P_i * Log (P_i)
+            entropy = tf.reduce_mean(self.openai_entropy(logits))
             
             # Policy Loss:  (1 / n) * ∑ * -log π(a_i|s_i) * A(s_i, a_i) 
-            # self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=actions_hot)
-            # self.policy_loss = tf.reduce_mean(neg_log_prob * advantages)
+            neg_log_prob = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=actions_hot)
+            policy_loss = tf.reduce_mean(neg_log_prob * advantages)
             
             # Value loss "MSE": (1 / n) * ∑[V(i) - R_i]^2
-            loss = tf.reduce_mean(tf.square(advantages)) 
-
-
-            # self.loss = self.policy_loss + (self.value_loss * self.value_function_coeff)
-            # params = self.trainable_variables
-            # params = advantages
-            # grads = tape.gradient(loss, params)            
-            # grads, global_norm = tf.clip_by_global_norm(grads, self.max_grad_norm)
+            value_loss = tf.reduce_mean(tf.square(advantages)) 
+ 
+            loss = policy_loss + (value_loss * self.value_function_coeff) - (entropy.numpy() * self.entropy_coef)
 
             # Apply Gradients
             optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, epsilon=self.epsilon)
             
-            grads = tape.gradient(loss, advantages)
-            # global_step=tf.train.get_or_create_global_step()
-            optimizer.apply_gradients(zip([grads], [advantages]))
-        
-            return loss.numpy()
-      
-            #self.optimizer.apply_gradients(zip(params, self.trainable_variables), global_step=tf.train.get_or_create_global_step())`
+            # grads = tape.gradient(loss, advantages)
+            grads = tape.gradient(loss, [logits, advantages])
 
+            grads, global_norm = tf.clip_by_global_norm(grads, self.max_grad_norm)
+
+            optimizer.apply_gradients(zip(grads, [logits, advantages]))
+        
+            return loss.numpy(), policy_loss.numpy(), value_loss.numpy(), entropy.numpy()
     
     # Makes a step in the environment
     def step(self, observation, keep_per):
-        
-        # softmax, value = self.sess.run([self.network.action_dist, self.network.value], {self.network.input_def: observation, self.network.keep_prob: 1.0})
         softmax, logits, value = self.call(observation, keep_per)
         return logits.numpy(), softmax.numpy(), value.numpy()
 
     # Returns the critic estimation of the current state value
     def value(self, observation):
-        # value = self.sess.run(self.network.value, {self.network.input_def: observation, self.network.keep_prob: 1.0})
         action_dist, softmax, value = self.call(observation, 1.0)
-        return value.numpy()
+        return value.numpy()[0]
 
     def save_model(self):
         current_dir = os.getcwd()
         model_save_path = current_dir + '\Model\checkpoint.h5'
-        self.model.save_weights(model_save_path)
+        self.save_weights(model_save_path)
 
     def load_model(self):
         current_dir = os.getcwd()
         model_save_path = current_dir + '\Model\checkpoint.h5'
         loaded_model = tf.keras.models.load_weights(model_save_path)
-        self.model.set_weights(loaded_model) 
+        self.set_weights(loaded_model) 
 
-
-
-class CustomDropout(tf.keras.layers.Layer):
-
-  def __init__(self, rate, **kwargs):
-    super(CustomDropout, self).__init__(**kwargs)
-    self.rate = rate
-
-#   def call(self, inputs, is_training=False, rate = self.rate):
-#     if is_training:
-#         return tf.nn.dropout(inputs, keep_prob=rate)
-#     return inputs
+    def openai_entropy(self, logits):
         
-        
+        # Entropy from OpenAI A2C baseline
+        a0 = logits - tf.reduce_max(logits, 1, keepdims=True)
+        ea0 = tf.exp(a0)
+        z0 = tf.reduce_sum(ea0, 1, keepdims=True)
+        p0 = ea0 / z0
+        return tf.reduce_sum(p0 * (tf.math.log(z0) - a0), 1)
 
-
-class AC_Network:
-#     def __init__(self, input_shape, num_actions, sess, is_training=True, model = None):
-#         self.input_shape = input_shape
-#         self.value_s = None
-#         self.action_s = None
-#         self.num_actions = num_actions
-#         self.sess = sess
-
-#         # Dicounting hyperparams for loss functions
-#         self.entropy_coef = 0.01
-#         self.value_function_coeff = 0.5
-#         self.max_grad_norm = 40.0
-#         self.learning_rate = 7e-4
-#         self.alpha = 0.99
-#         self.epsilon = 1e-5
-
-#         # Model variables
-#         (_, hight, width, stack) = self.input_shape
-#         self.input_def = tf.keras.layers.Input(shape=(hight, width*stack, 1), name="input_layer", dtype=tf.float32)
-#         self.keep_prob = tf.placeholder(tf.float32, (None), "keep_prob")
-
-#         # Define Convolution 1
-#         self.conv1 = tf.keras.layers.Conv2D(
-#             filters=32,
-#             kernel_size=[5, 5],
-#             kernel_initializer=keras.initializers.Orthogonal(gain=.7, seed=None),
-#             padding="valid",
-#             activation="relu", 
-#             name="conv1")
-        
-#         self.maxPool1 = tf.keras.layers.MaxPooling2D(pool_size = (3, 3), name = "maxPool1")
-#         self.dropout = tf.keras.layers.Dropout(self.keep_prob)
-
-#         # define Convolution 2
-#         self.conv2 = tf.keras.layers.Conv2D(
-#             filters=64,
-#             kernel_size=[3, 3],
-#             kernel_initializer=keras.initializers.Orthogonal(gain=.7, seed=None),
-#             padding="valid",
-#             activation="relu",
-#             name="conv2")
-
-#         self.maxPool2 = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), name="maxPool2")
-        
-#         # define Convolution 3
-#         self.conv3 = tf.keras.layers.Conv2D(
-#             filters=96,
-#             kernel_size=[2, 2],
-#             kernel_initializer=keras.initializers.Orthogonal(gain=.7, seed=None),
-#             padding="valid",
-#             activation="relu",
-#             name="conv3")
-
-#         self.maxPool3 = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), name ="maxPool3")
-#         self.flattened = tf.keras.layers.Flatten(name="flattening_layer")
-#         self.hiddenLayer = tf.keras.layers.Dense(
-#             512,
-#             activation="relu",
-#             # bias_regularizer=tf.keras.regularizers.l2(0.01),
-#             kernel_initializer=keras.initializers.Orthogonal(gain=.7, seed=None),
-#             name="hidden_layer")
-
-#         # Output Layer consisting of an Actor and a Critic
-#         self._value = tf.keras.layers.Dense(
-#             1,
-#             kernel_initializer=keras.initializers.Orthogonal(gain=2, seed=None),
-#             activation='linear',
-#             name="value_layer")
-   
-#         self._policy = tf.keras.layers.Dense(
-#             7,
-#             activation='linear',
-#             kernel_initializer=keras.initializers.Orthogonal(gain=.7, seed=None),
-#             name="policy_layer")
-
-#         self.batch_normalization1 = tf.keras.layers.BatchNormalization()
-#         self.batch_normalization2 = tf.keras.layers.BatchNormalization()
-#         self.batch_normalization3 = tf.keras.layers.BatchNormalization()
-
-#         # Define the forward pass for the convolutional and hidden layers
-#         conv1_out = self.conv1(self.input_def)
-#         conv1_out = self.batch_normalization1(conv1_out) 
-#         maxPool1_out = self.maxPool1(conv1_out)
-
-#         conv2_out = self.conv2(maxPool1_out)
-#         conv2_out = self.batch_normalization2(conv2_out) 
-#         maxPool2_out = self.maxPool2(conv2_out)
-
-#         conv3_out = self.conv3(maxPool2_out)
-#         conv3_out = self.batch_normalization3(conv3_out) 
-#         maxPool3_out = self.maxPool3(conv3_out)
-        
-#         flattened_out = self.flattened(maxPool3_out)
-#         hidden_out = self.dropout(flattened_out)
-
-#         # Actor and the Critic outputs
-#         self.value = self._value(hidden_out)
-#         self.logits = self._policy(hidden_out)
-#         self.action_dist = tf.nn.softmax(self.logits)
-
-#         # Final model
-#         self.model = tf.keras.Model(inputs=[self.input_def, ], outputs=[self.logits, self.value])
-
-#         # have to initialize before threading
-#         self.model._make_predict_function()
-    
-#         # Batch data that will be sent to Model by the coordinator
-#         self.actions = tf.keras.backend.placeholder(dtype=tf.int32, shape=[None])
-#         self.actions_hot = tf.one_hot(self.actions, 7, dtype=tf.float32)
-#         self.advantages = tf.keras.backend.placeholder(dtype=tf.float32, shape=[None])
-#         self.rewards = tf.keras.backend.placeholder(dtype=tf.float32, shape=[None])
-#         self.values = tf.keras.backend.placeholder(dtype=tf.float32, shape=[None])
-        
-#         # Responsible Outputs -log π(a_i|s_i)i
-#         # self.neg_log_prob = tf.reduce_sum(self.logits * self.actions_hot + 1e-10, [1])
-#         # self.policy_loss = tf.reduce_mean(-1.0 * tf.log(self.neg_log_prob) * self.advantages)
-
-#         # Policy Loss:  (1 / n) * ∑ * -log π(a_i|s_i) * A(s_i, a_i) 
-#         self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.actions_hot)
-#         self.policy_loss = tf.reduce_mean(self.neg_log_prob * self.advantages)
-        
-#         # Value loss "MSE": (1 / n) * ∑[V(i) - R_i]^2
-#         self.value_loss = tf.reduce_mean(tf.square(self.rewards - self.values)) 
-
-#         # Entropy: - (1 / n) * ∑ P_i * Log (P_i)
-#         # self.entropy = - tf.reduce_mean(self.logits * tf.log(self.logits + 1e-10))
-#         # self.entropy = tf.reduce_mean(openai_entropy(self.logits))
-
-#         # Total loss: Policy loss - entropy * entropy coefficient + value coefficient * value loss
-#         #  - (self.entropy * self.entropy_coef)
-#         self.loss = self.policy_loss + (self.value_loss * self.value_function_coeff)
-#         params = self.model.trainable_weights
-#         grads = tf.gradients(self.loss, params)            
-#         grads, self.global_norm = tf.clip_by_global_norm(grads, self.max_grad_norm)
-
-#         # Apply Gradients
-#         optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate, epsilon=self.epsilon)
-        
-#         # Update network weights
-#         self.optimize = optimizer.apply_gradients(zip(grads, params))
-
-            
+    def softmax_entropy(self, p0):
+        # standard entropy
+        return - tf.reduce_sum(p0 * tf.log(p0 + 1e-6), axis=1)

Coordinator.py
@@ -7,8 +7,7 @@ import tensorflow.keras as keras
 import tensorflow.keras.backend as k
 
 # import local classes
-from Model import Model
-from AC_Network import AC_Network
+from AC_Network import AC_Model
 from Worker import Worker, WorkerThread
 
 
@@ -41,9 +40,12 @@ class Coordinator:
        
     def train(self, train_data):
 
-        loss = self.global_model.train_batch(train_data)
+        loss, policy_loss, value_loss, entropy = self.global_model.train_batch(train_data)
         
         self.plot.collector.collect("LOSS", loss)
+        self.plot.collector.collect("VALUE_LOSS", value_loss)
+        self.plot.collector.collect("POLICY_LOSS", policy_loss)
+        self.plot.collector.collect("ENTROPY", entropy)
 
     # Produces reversed list of discounted rewards
     def discount(self, x, gamma):
@@ -99,6 +101,7 @@ class Coordinator:
                     all_rewards = np.array([])
                     all_states = np.array([])
                     all_actions = np.array([])
+                    all_logits = np.array([])
 
                     # Calculate discounted rewards for each environment
                     for env in range(self.num_envs):
@@ -109,6 +112,7 @@ class Coordinator:
                         batch_observations = []
                         batch_states = []
                         batch_actions = []
+                        batch_logits = []
 
                         mb = batches[env]
                         
@@ -123,12 +127,13 @@ class Coordinator:
                         observation = None
 
                         for step in mb:
-                            (state, observation, reward, value, action, done) = step
+                            (state, observation, reward, value, action, done, logits) = step
                             batch_rewards.append(reward)
                             batch_values.append(value)
                             batch_observations.append(observation)
                             batch_states.append(state)
                             batch_actions.append(action)
+                            batch_logits.append(logits)
 
                         
                         # If we reached the end of an episode or if we filled a batch without reaching termination of episode
@@ -161,6 +166,7 @@ class Coordinator:
                             all_states = np.concatenate((all_states, batch_states), 0) if all_states.size else np.array(batch_states)
                             all_actions = np.concatenate((all_actions, batch_actions), 0) if all_actions.size else np.array(batch_actions)
                             all_values = np.concatenate((all_values, batch_values), 0) if all_values.size else np.array(batch_values)
+                            all_logits = np.concatenate((all_logits, batch_logits), 0) if all_logits.size else np.array(batch_logits)
                         
                         else:
                             
@@ -176,11 +182,11 @@ class Coordinator:
                             all_states = np.concatenate((all_states, batch_states), 0) if all_states.size else np.array(batch_states)
                             all_actions = np.concatenate((all_actions, batch_actions), 0) if all_actions.size else np.array(batch_actions)
                             all_values = np.concatenate((all_values, batch_values), 0) if all_values.size else np.array(batch_values)
-
+                            all_logits = np.concatenate((all_logits, batch_logits), 0) if all_logits.size else np.array(batch_logits)
 
                     # We can do this because: d/dx ∑ loss  == ∑ d/dx loss
                     batch_states = np.array(batch_states)
-                    data = (all_states, all_actions, all_advantages, all_rewards, all_values)
+                    data = (all_states, all_actions, all_advantages, all_rewards, all_values, all_logits)
                     
                     if data[0].size != 0:
                         self.train(data) 

Worker.py
@@ -69,12 +69,12 @@ class Worker():
             # Make a prediction and take a step if the epoc is not done
             if not self._done:
                 self.total_steps += 1
-                _, [actions_dist], [value] = self.network.step(np.expand_dims(self.s, axis=0), self._keep_prob())
+                [logits], [actions_dist], [[value]] = self.network.step(np.expand_dims(self.s, axis=0), self._keep_prob())
                 action = self.action_select(actions_dist)
                 [s_t], reward, d, _ = self.env.step(action)
                 self._done = d
 
-                batch.append((self.s, s_t , reward, value, action, d))
+                batch.append((self.s, s_t , reward, value, action, d, logits.tolist()))
                 s = [s_t]
 
                 # render the env