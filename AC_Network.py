Added more stat measures, fixed error in epsilon greedy calculation.

AC_Network.py
@@ -29,7 +29,7 @@ class AC_Model(tf.keras.Model):
             128,
             activation="relu",
             kernel_initializer=tf.initializers.lecun_uniform(),
-            kernel_regularizer=keras.regularizers.l2(l=0.01),
+            #kernel_regularizer=keras.regularizers.l2(l=0.01),
             name="hidden_layer1", 
             use_bias=True,
             dtype="float64",
@@ -39,7 +39,7 @@ class AC_Model(tf.keras.Model):
             128,
             activation="relu",
             kernel_initializer=tf.initializers.lecun_uniform(),
-            kernel_regularizer=keras.regularizers.l2(l=0.01),
+            #kernel_regularizer=keras.regularizers.l2(l=0.01),
             name="hidden_layer2", 
             use_bias=True,
             dtype="float64",
@@ -49,7 +49,7 @@ class AC_Model(tf.keras.Model):
             128,
             activation="relu",
             kernel_initializer=tf.initializers.lecun_uniform(),
-            kernel_regularizer=keras.regularizers.l2(l=0.01),
+            #kernel_regularizer=keras.regularizers.l2(l=0.01),
             name="hidden_layer3", 
             use_bias=True,
             dtype="float64",
@@ -75,7 +75,7 @@ class AC_Model(tf.keras.Model):
         # self.LN3 = tf.keras.layers.LayerNormalization()
         # self.LN4 = tf.keras.layers.LayerNormalization()
 
-        self.lstm = tf.keras.layers.SimpleRNN(128, trainable=is_training, dtype=tf.float64)
+        # self.lstm = tf.keras.layers.SimpleRNN(128, trainable=is_training, dtype=tf.float64)
 
         # Output Layer consisting of an Actor and a Critic
         self._value = tf.keras.layers.Dense(

Worker.py
@@ -56,7 +56,7 @@ class Worker():
                 [logits], [action_dist], value = self.network.step(np.expand_dims(self.s, axis=0), keep_prob)
 
                 # temperature=(1 - keep_prob) * 10
-                action = self.action_select(logits, exploration="Epsilon_greedy", temperature=1 - keep_prob)
+                action = self.action_select(logits, exploration="Epsilon_greedy", temperature=(1 - keep_prob))
                 s_t, reward, d, stuff = self.env.step(action)
                 self._done = d
 
@@ -84,12 +84,12 @@ class Worker():
             
 
     # Boltzmann Softmax style action selection
-    def action_select(self, dist, exploration="boltzmann", temperature=1.0, epsilon=.05):
+    def action_select(self, dist, exploration="boltzmann", temperature=1.0, epsilon=.1):
         
 
         if exploration == "boltzmann":        
             
-            dist = tf.nn.softmax(dist / temperature).numpy()
+            dist = tf.nn.softmax(dist / ((temperature) * 10)).numpy()
             a = np.random.choice(dist,p=dist)
             probs = dist == a
             a = np.argmax(probs)
@@ -109,10 +109,7 @@ class Worker():
                 return random.randint(0, self.NUM_ACTIONS-1)
 
             else:
-                # scale temperature from 9 to 1
-                dist = tf.nn.softmax(dist / ((temperature) * 10)).numpy() 
-                a = np.random.choice(dist,p=dist)
-                probs = dist == a
-                a = np.argmax(probs)
+                dist = tf.nn.softmax(dist).numpy() 
+                a = np.argmax(dist)
                 return a
 